9f1683092723c99dc1aed9be6ef035feaa00dd6e
SethminiDissanayake <sethminidissa@gmail.com>
2023-08-28 16:51:17 +0530

    Login_Form fxml file is completed

--- src/lk/ijse/hostal_management_system/AppInitializer.java
+++ src/lk/ijse/hostal_management_system/AppInitializer.java
@@ -1,6 +1,22 @@
 package lk.ijse.hostal_management_system;
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
+public class AppInitializer extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent =  FXMLLoader.load(getClass().getResource("/resources/view/Login_Form.fxml"));
+        stage.setScene(new Scene(parent));
+        stage.setTitle("D24 Hostel Management System");
+        stage.centerOnScreen();
+
+        stage.show();
+
+    }
 }
--- src/lk/ijse/hostal_management_system/controller/LoginFormController.java
+++ src/lk/ijse/hostal_management_system/controller/LoginFormController.java
@@ -1,5 +1,29 @@
 package lk.ijse.hostal_management_system.controller;
 
-public class LoginFormController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-}
+    public class LoginFormController {
+        @FXML
+        private Button btnLogin;
+
+        @FXML
+        private TextField txtPassowrd;
+
+        @FXML
+        private TextField txtUsername;
+
+        @FXML
+        void LoginButtonOnAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void ToggleButtonOnAction(ActionEvent event) {
+
+        }
+
+    }
+
