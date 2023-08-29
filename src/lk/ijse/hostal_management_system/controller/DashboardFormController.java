7dbd38d9a3347b7dd6526082ec4d90172c88c66f
SethminiDissanayake <sethminidissa@gmail.com>
2023-08-29 12:03:23 +0530

    Create Student_form fxml File

--- src/lk/ijse/hostal_management_system/controller/DashboardFormController.java
+++ src/lk/ijse/hostal_management_system/controller/DashboardFormController.java
@@ -2,6 +2,13 @@ package lk.ijse.hostal_management_system.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardFormController {
 
@@ -17,8 +24,13 @@ public class DashboardFormController {
     }
 
     @FXML
-    void StudentButtonOnAction(ActionEvent event) {
+    void StudentButtonOnAction(ActionEvent event) throws IOException {
 
+        Parent dashboardParent = FXMLLoader.load(getClass().getResource("/resources/view/Dashboard_Form.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene dashboardScene = new Scene(dashboardParent);
+        stage.setScene(dashboardScene);
+        stage.show();
     }
 
     @FXML
