e376b36c2214d7d0aa6d65ba4be78b2022d701c1
SethminiDissanayake <sethminidissa@gmail.com>
2023-08-30 12:34:45 +0530

    ReservationDTO  Created

--- src/lk/ijse/hostal_management_system/dto/ReservationDTO.java
+++ src/lk/ijse/hostal_management_system/dto/ReservationDTO.java
@@ -1,4 +1,21 @@
 package lk.ijse.hostal_management_system.dto;
 
+import lk.ijse.hostal_management_system.entity.Room;
+import lk.ijse.hostal_management_system.entity.Student;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReservationDTO {
+    private String res_id;
+    private LocalDate res_date;
+    private String status;
+
+    private Student student;
+    private Room room;
 }
