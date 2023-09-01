1bac2766e05a6e67e30120dfec1478572f30eb83
SethminiDissanayake <sethminidissa@gmail.com>
2023-09-01 12:41:24 +0530

    RoomDAO interface created

---
+++ src/lk/ijse/hostal_management_system/dao/custom/RoomDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.hostal_management_system.dao.custom;
+
+import lk.ijse.hostal_management_system.dao.CrudDAO;
+import lk.ijse.hostal_management_system.entity.Room;
+
+public interface RoomDAO extends CrudDAO<Room> {
+}
