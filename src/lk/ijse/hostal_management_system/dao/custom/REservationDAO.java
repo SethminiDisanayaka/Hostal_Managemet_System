349552cd399c11f89b2f8ae56c5fd070369b3038
SethminiDissanayake <sethminidissa@gmail.com>
2023-09-03 14:54:16 +0530

    ReservationDAOImpl class created

--- src/lk/ijse/hostal_management_system/dao/custom/REservationDAO.java
+++
@@ -1,4 +0,0 @@
-package lk.ijse.hostal_management_system.dao.custom;
-
-public interface REservationDAO {
-}
---
+++ src/lk/ijse/hostal_management_system/dao/custom/ReservationDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.hostal_management_system.dao.custom;
+
+import lk.ijse.hostal_management_system.dao.CrudDAO;
+import lk.ijse.hostal_management_system.entity.Reservation;
+
+public interface ReservationDAO extends CrudDAO<Reservation> {
+}
