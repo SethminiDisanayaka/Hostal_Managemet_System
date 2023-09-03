ab51cb6e43f9b32f05b7e0b7a10d39784bf51101
SethminiDissanayake <sethminidissa@gmail.com>
2023-09-03 15:03:09 +0530

    CustomeDTO class created

---
+++ src/lk/ijse/hostal_management_system/bo/custom/ReservationBO.java
@@ -0,0 +1,6 @@
+package lk.ijse.hostal_management_system.bo.custom;
+
+import lk.ijse.hostal_management_system.bo.SuperBO;
+
+public interface ReservationBO extends SuperBO {
+}
---
+++ src/lk/ijse/hostal_management_system/dto/CustomDTO.java
@@ -0,0 +1,46 @@
+package lk.ijse.hostal_management_system.dto;
+
+import lk.ijse.hostal_management_system.entity.Reservation;
+import lk.ijse.hostal_management_system.entity.Room;
+import lk.ijse.hostal_management_system.entity.Student;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomDTO {
+
+    // reservation
+    private String res_id;
+    private LocalDate res_date;
+    private String status;
+    private Student student;
+    private Room room;
+
+    // Room
+    private String room_type_id;
+    private String type;
+    private String key_money;
+    private int qty;
+    private List<Reservation> reservationList = new ArrayList<>();
+
+    // Student
+    private String id;
+    private String name;
+    private String address;
+    private String contact_no;
+    private String dob;
+    private String gender;
+
+
+    public CustomDTO(String res_id, LocalDate res_date, String room_type_id,  String type,String id, String name,String key_money,String status ) {
+        this.res_id = res_id;
+        this.res_date = res_date;
+        this.status = status;
+        this.room_type_id = room_type_id;
+        this.type = type;
+        this.key_money = key_money;
+        this.id = id;
+        this.name = name;
+    }
+}
