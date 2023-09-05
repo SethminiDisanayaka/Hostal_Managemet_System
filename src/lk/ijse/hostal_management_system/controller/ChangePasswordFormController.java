a50f36198e8dddfddd2bbb791bfe721626783d8e
SethminiDissanayake <sethminidissa@gmail.com>
2023-09-05 23:31:01 +0530

    UserForm fxml file created

---
+++ src/lk/ijse/hostal_management_system/controller/ChangePasswordFormController.java
@@ -0,0 +1,45 @@
+package lk.ijse.hostal_management_system.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ChangePasswordFormController {
+
+    @FXML
+    private Button btnchange;
+
+    @FXML
+    private Button btnconfirm;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private AnchorPane pane2;
+
+    @FXML
+    private TextField txtpassword;
+
+    @FXML
+    private TextField txtpasswordnew;
+
+    @FXML
+    private TextField txtusername;
+
+    @FXML
+    private TextField txtusernamenew;
+
+    @FXML
+    void changeButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmButtonOnAction(ActionEvent event) {
+
+    }
+
+}
