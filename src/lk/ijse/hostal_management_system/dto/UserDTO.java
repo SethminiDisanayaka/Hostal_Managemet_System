b34cb752f48b2bb9bb7dc80b974797d9d481f88c
SethminiDissanayake <sethminidissa@gmail.com>
2023-09-05 23:34:46 +0530

    UserDTO created

---
+++ src/lk/ijse/hostal_management_system/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package lk.ijse.hostal_management_system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String id;
+    private String name;
+    private String password;
+}
